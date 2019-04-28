9795f3f519b5cf4c2819b16e6c5bc5f9232d4db3
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2019-04-28 19:01:26 +0200

    js & navbar fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,24 +36,24 @@ $(window).on("load", function() {
 
 
 
-const nav = $("#navigation");
-const navTop = nav.offset().top;
-
-$("#navigation li a").click(function(e) {
-  e.preventDefault();
-
-  var targetElement = $(this).attr("href");
-  var targetPosition = $(targetElement).offset().top;
-  $("html, body").animate({ scrollTop: targetPosition - 79}, "slow");
-});
-
-var randomimage = faker.image.image(); // Caitlyn Kerluke
-
-var matches = document.querySelectorAll("div.imgBx > img");
-
-for (var i = 0; i<matches.length; i++) {
-  matches[i].src = randomimage;
-};
+// const nav = $("#navigation");
+// const navTop = nav.offset().top;
+//
+// $("#navigation li a").click(function(e) {
+//   e.preventDefault();
+//
+//   var targetElement = $(this).attr("href");
+//   var targetPosition = $(targetElement).offset().top;
+//   $("html, body").animate({ scrollTop: targetPosition - 79}, "slow");
+// });
+//
+// var randomimage = faker.image.image(); // Caitlyn Kerluke
+//
+// var matches = document.querySelectorAll("div.imgBx > img");
+//
+// for (var i = 0; i<matches.length; i++) {
+//   matches[i].src = randomimage;
+// };
 
 var url = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffeed.pippa.io%2Fpublic%2Fshows%2Fwords-matter";
 fetch(url)
